5ebbfe8d9d0f8d7ea646142fec66f3cf5fbe056c
unknown <anju7295>
2023-10-11 05:00:24 +0530

    first commit

---
+++ backend/src/user/userModel.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    }
+});
+module.exports = mongoose.model('employees', userSchema);
